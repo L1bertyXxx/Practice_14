19a24fcc7677b140104720d9206288f26b4e76e8
L1berty <larsanik92@gmail.com>
2025-05-25 07:39:08 +0300

    task_1_1

--- task_1.py
+++ task_1.py
@@ -12,7 +12,6 @@ class Restaurant:
     def open_restaurant(self):
         print(f"Ресторан {self.restaurant_name} открыт!")
 
-
 class IceCreamStand(Restaurant):
     def __init__(self, restaurant_name, cuisine_type, rating, flavors):
         super().__init__(restaurant_name, cuisine_type, rating)
